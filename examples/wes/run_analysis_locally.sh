79a520daf405fcf9365b369ed9db1ebf8a03c9cd
peter-yufan-zeng <zyfniu@hotmail.com>
2019-11-27 00:41:41 -0500

    Update

--- examples/wes/run_analysis_locally.sh
+++ examples/wes/run_analysis_locally.sh
@@ -2,4 +2,5 @@
 
 conda activate NGS-pipe
 module load annovar trimmomatic fastqc gatk picard bwa samtools bcftools bedtools gatk/3.7
+gatk-register /home/zyfniu/projects/def-nicholsa/zyfniu/NGS-pipe-index-resources/GenomeAnalysisTK.jar
 snakemake -s SRP051153.snake --configfile config.json -p
